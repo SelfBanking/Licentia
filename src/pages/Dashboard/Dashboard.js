4db3246e4c4625b5a21d7e49e35f5393695aaf65
Chad Lynch <chyad@hotmail.com>
2020-05-16 01:07:17 +0800

    cleaned up, fixed routing

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,25 +1,47 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { Grid, Paper, Typography } from '@material-ui/core'
+import useStyles from './styles'
+import Mainchart from './components/Mainchart'
+import AccountDetails from './components/AccountDetails'
 
-import { Grid, Box } from '@material-ui/core'
-
-import Aave from './components/Aave'
-import Compound from './components/Compound'
-import HeadlineMenu from './components/HeadlineMenu'
+import ReactFlagsSelect from 'react-flags-select'
+import 'react-flags-select/css/react-flags-select.css'
 
 function Dashboard (props) {
+  const classes = useStyles()
+
   return (
-    <Grid container justify='center' spacing={3}>
-      <HeadlineMenu {...props} />
-      <Grid item md={6}>
-        <Box m={3}>
-          <Aave {...props} />
-        </Box>
+    <Grid container spacing={1} className={classes.mainGrid} align='center'>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Grid item xs={6}>
+            <h2>Total Balance</h2>
+          </Grid>
+          <Grid item xs={6}>
+            <ReactFlagsSelect
+              className={classes.flagDropdown}
+              defaultCountry='US'
+              countries={['US', 'GB', 'FR', 'DE', 'IT', 'NG']}
+            />
+            <Typography variant='h1' component='h2'>
+              $ 125,00
+            </Typography>
+          </Grid>
+        </Paper>
       </Grid>
-      <Grid item md={6}>
-        <Box m={3}>
-          <Compound {...props} />
-        </Box>
+      <Grid item xs={12} sm={8}>
+        <Paper className={classes.paper}>
+          <Mainchart />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Typography variant='h3' gutterBottom>
+          ALL ASSETS
+        </Typography>
+        <Paper className={classes.paper}>
+          <AccountDetails />
+        </Paper>
       </Grid>
     </Grid>
   )
--- src/pages/Overview/Overview.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-
-const Overview = props => {
-  return <p>Overview Page</p>
-}
-
-export default withRouter(Overview)
