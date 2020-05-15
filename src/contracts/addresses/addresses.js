175ae59a8e317f90f3cea447f761c5e526ba3b7b
Chad Lynch <chyad@hotmail.com>
2020-05-16 02:02:48 +0800

    added some uniswap abis

--- src/contracts/addresses/addresses.js
+++ src/contracts/addresses/addresses.js
@@ -8,3 +8,11 @@ export const aETHaddress = {
   kovan: '',
   mainnet: '0x3a3A65aAb0dd2A17E3F1947bA16138cd37d08c04'
 }
+
+export const uniswap = {
+  factory: '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
+  ropsten: '0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351',
+  rinkeby: '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',
+  kovan: '0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30',
+  görli: '0x6Ce570d02D73d4c384b46135E87f8C592A8c86dA'
+}
