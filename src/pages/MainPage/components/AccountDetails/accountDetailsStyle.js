4c36a691a31e5ddd93e901119211b1c8c65570a2
Pablo <pabrandenburg@gmail.com>
2020-05-14 20:22:24 -0500

    created two static account block of info on the right side, for feedback and so we can start hooking account info to it

--- src/pages/MainPage/components/AccountDetails/accountDetailsStyle.js
+++ src/pages/MainPage/components/AccountDetails/accountDetailsStyle.js
@@ -1,6 +1,35 @@
 import { makeStyles } from '@material-ui/core'
 
 export default makeStyles(theme => ({
-    AccountDetailsStyle:{
-    }
+    CardRoot: {
+        backgroundColor:'#e5e5e5',
+        opacity:'80%',
+        height: '140px',
+        width: '400px'
+      },
+      bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+      },
+      pos: {
+        marginBottom: 12,
+      },
+      title: {
+        fontSize: 20,
+        fontStyle: 'bold'
+      },
+      amount: {
+        fontSize: 16,
+        fontStyle: 'bold'
+      },
+      coinworth: {
+        fontSize: 20,
+        fontStyle: 'bold'
+      },
+      gain: {
+        fontSize: 16,
+        fontStyle: 'bold',
+        color: 'green'
+      },
 }))
