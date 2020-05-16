dc69ca6a082b077a2c1eb12d98a464428f586bbe
Pablo <pabrandenburg@gmail.com>
2020-05-16 06:59:02 -0500

    forgot had to comment out header on app so no yarn warning

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, HashRouter } from 'react-router-dom'
 
-import Header from '../../components/Header'
+//import Header from '../../components/Header'
 import Dashboard from '../Dashboard'
 
 function App (props) {
